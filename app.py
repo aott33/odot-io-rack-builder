90ba988a1306a646a75925c3c74922164f1f1c7b
Andrew Ott <ott.andrew33@gmail.com>
2024-01-02 06:15:54 -0800

    Started working on flask tutorial

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello, world")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, Andrew!</p>"
+
+if __name__ == "__main__":
+    print("I am inside the if statement")
+    app.run("0.0.0.0", debug=True)
