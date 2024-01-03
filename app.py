df705dd6b5898c29d6fb91f8f1a03315bddd6112
Andrew Ott <ott.andrew33@gmail.com>
2024-01-03 15:19:58 -0800

    Updated the home.html file and rendered it in app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Andrew!</p>"
+    return render_template('home.html')
 
 if __name__ == "__main__":
     print("I am inside the if statement")
